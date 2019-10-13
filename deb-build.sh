de85987fc4b6d4159b9a8d83db7239f8b560491e
along <anlaneg@126.com>
2019-10-13 09:43:51 +0800

    add unstable version support

--- deb-build.sh
+++ deb-build.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
+VERSION="`git log --pretty=format:\"%h\" -1`"
+DEBEMAIL="owner <owner@example.com>"
+export DEBEMAIL
+dch -D unstable -i "New unstable version:$VERSION"
 DEB_BUILD_OPTIONS='parallel=8 nocheck’ dpkg-buildpackage -b -us -uc -d
