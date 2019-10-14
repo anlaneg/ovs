7cb656908034f1b24e05972990bb6413cbfc462b
anlaneg <anlaneg@126.com>
2019-10-14 00:57:58 +0000

    update deb-build.sh

--- deb-build.sh
+++ deb-build.sh
@@ -2,5 +2,6 @@
 VERSION="`git log --pretty=format:\"%h\" -1`"
 DEBEMAIL="owner <owner@example.com>"
 export DEBEMAIL
+export CFLAGS='-g -O0'
 dch -D unstable -i "New unstable version:$VERSION"
-DEB_BUILD_OPTIONS='parallel=8 nocheck’ dpkg-buildpackage -b -us -uc -d
+DEB_BUILD_OPTIONS='parallel=8 nocheck' dpkg-buildpackage -b -us -uc -d
